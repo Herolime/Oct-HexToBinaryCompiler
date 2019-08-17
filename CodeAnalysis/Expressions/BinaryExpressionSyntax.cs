8c02f6223b6cfb962edfae05aa78182f22c926f1
Herolime <emilesanen@gmail.com>
2019-08-16 17:46:35 -0700

    Add files via upload

---
+++ CodeAnalysis/Expressions/BinaryExpressionSyntax.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using OctAndHexToBinaryCompiler.CodeAnalysis.SyntaxAnalysis;
+
+namespace OctAndHexToBinaryCompiler.CodeAnalysis.Expressions
+{
+    public sealed class BinaryExpressionSyntax : ExpressionSyntax
+    {
+        public BinaryExpressionSyntax(ExpressionSyntax left, SyntaxToken operatorToken, ExpressionSyntax right) 
+        {
+            Left = left;
+            OperatorToken = operatorToken;
+            Right = right;
+        }
+
+        public ExpressionSyntax Left { get; }
+        public SyntaxToken OperatorToken { get; }
+        public ExpressionSyntax Right { get; }
+
+        public override SyntaxKind Kind => SyntaxKind.BinaryExpression;
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return Left;
+            yield return OperatorToken;
+            yield return Right;
+        }
+    }
+}
