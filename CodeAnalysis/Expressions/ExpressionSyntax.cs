8b756af16f0801c71dc974aa9acd3bb47a6bc5c1
Herolime <emilesanen@gmail.com>
2019-08-16 17:45:39 -0700

    Create ExpressionSyntax.cs

---
+++ CodeAnalysis/Expressions/ExpressionSyntax.cs
@@ -0,0 +1,9 @@
+using OctAndHexToBinaryCompiler.CodeAnalysis.SyntaxAnalysis;
+
+namespace OctAndHexToBinaryCompiler.CodeAnalysis.Expressions
+{
+    public abstract class ExpressionSyntax: SyntaxNode
+    {
+        
+    }
+}
